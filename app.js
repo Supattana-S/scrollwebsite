dde069ebc0b4cc94d51af27c0f29833cc0a6b294
Supattana-S <supattana.sue@gmail.com>
2022-06-10 10:35:37 +0700

    JS: Create highlightMenu()

--- app.js
+++ app.js
@@ -10,3 +10,18 @@ const mobileMenu = () => {
 
 menu.addEventListener("click", mobileMenu);
 
+// Show active menu when scrolling
+
+const highlightMenu = () => {
+  // const highlight = document.querySelector(".highlight");
+  // const homeMenu = document.querySelector("#home-page");
+  // const aboutMenu = document.querySelector("#about-page");
+  // const servicesMenu = document.querySelector("#services-page");
+
+  // let scrollPos = window.scrollY
+
+  // // adds 'highlight' class to my menu items
+  // // only desktop
+
+  // if(window.innerWidth > 960 && scrollPos < 600)
+};
